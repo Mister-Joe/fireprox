6a8a3bd87390b999e26580456d988ee13e96cb35
Mister-Joe <joseph.tredeau@outlook.com>
2025-07-24 11:55:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="fireprox",
     version="1.0.0",
     packages=find_packages(),
-    py_modules=["fireprox"],
+    py_modules=["fire"],
     install_requires=[ "boto3", "tldextract", "tzlocal", "bs4", "lxml" ],
     entry_points={ "console_scripts": [ "fire=fire:main" ] }
 )
